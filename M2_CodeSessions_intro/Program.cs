69cabf4a836f7bc3ad2030f3174efb9e231afe4b
Martin <martin_lenart@icloud.com>
2023-09-04 14:04:59 +0200

    update

--- M2_CodeSessions_intro/Program.cs
+++ M2_CodeSessions_intro/Program.cs
@@ -1,3 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
+string fruit_name1 = "Banana";
+double fruit_weigth1 = 1.5;
+decimal fruit_price1 = 23.95M;
+
+string fruit_name2 = "Apelsin";
+double fruit_weigth2 = 2.5;
+decimal fruit_price2 = 15.80M;
+
+Console.WriteLine($"Total price for {fruit_weigth1}kg {fruit_name1} is {(decimal)fruit_weigth1 * fruit_price1}");
+Console.WriteLine($"Total price for {fruit_weigth2}kg {fruit_name2} is {(decimal)fruit_weigth2 * fruit_price2}");
+
+Console.WriteLine("\n\nFruit struct");
+Fruit fruit1 = new Fruit();
+fruit1.Name = "Grapes";
+fruit1.Weigth = 0.4;
+fruit1.Price = 12.30M;
+
+Fruit fruit2 = new Fruit();
+fruit2.Name = "Grapes";
+fruit2.Weigth = 0.4;
+fruit2.Price = 12.30M;
+
+Fruit fruit3 = fruit1;
+fruit1.Name = "Olives";
+
+Console.WriteLine($"Total price for {fruit1.Weigth}kg {fruit1.Name} is {(decimal)fruit1.Weigth * fruit1.Price}");
+Console.WriteLine($"Total price for {fruit2.Weigth}kg {fruit2.Name} is {(decimal)fruit2.Weigth * fruit2.Price}");
+Console.WriteLine($"Total price for {fruit3.Weigth}kg {fruit3.Name} is {(decimal)fruit3.Weigth * fruit3.Price}");
+
+
+Console.ReadKey();
+
+
+struct Fruit
+{
+    public string Name;
+    public double Weigth;
+    public decimal Price;
+}
---
+++ array/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace array;
+
+class Program
+{
+
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        //discrete
+        int i0 = 1;
+        int i1 = 2;
+        int i2 = 3;
+        int i3 = 4;
+
+        Console.WriteLine(i0);
+        Console.WriteLine(i1);
+        Console.WriteLine(i2);
+        Console.WriteLine(i3);
+
+        //array
+        int[] i_array = { 1, 2, 3, 4 };
+        Console.WriteLine(i_array[0]);
+        Console.WriteLine(i_array[1]);
+        Console.WriteLine(i_array[2]);
+        Console.WriteLine(i_array[3]);
+
+
+        int k = 3;
+        Console.WriteLine(i_array[k]);
+
+        for (int i = 0; i < i_array.Length; i++)
+        {
+            Console.WriteLine(i_array[i]);
+        }
+        Console.ReadKey();
+
+    }
+}
+
