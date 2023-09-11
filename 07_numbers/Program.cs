debd6e1838e8a41372b1a133056fba3183f83d90
Martin <martin_lenart@icloud.com>
2023-09-11 11:46:31 +0200

    updated 07_numbers

--- 07_numbers/Program.cs
+++ 07_numbers/Program.cs
@@ -1,14 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Enter numbers separated by ; (1; 2; 4; )");
+Console.WriteLine("Enter numbers separated by semicolon ex. (1;2;4)");
 
 string strInput = Console.ReadLine();
 string[] arrInput = strInput.Split(";");
 
 int sum = 0;
+int sumOdd = 0;
+int sumEven = 0;
 for (int i = 0; i < arrInput.Length-1; i++)
 {
     int intVal = int.Parse(arrInput[i]);
     sum = sum + intVal;
+
+    if ((intVal % 2) == 0)
+    {
+        //Console.WriteLine($"{intVal} is even");
+        sumEven += intVal;
+    }
+    else
+    {
+        //Console.WriteLine($"{intVal} is odd");
+        sumOdd = sumOdd + intVal;
+    }
 }
 
 Console.WriteLine($"The sum is {sum}");
+Console.WriteLine($"The odd sum is {sumOdd}");
+Console.WriteLine($"The even sum is {sumEven}");
