5606efe0c71630cd85d8394eb639d844fd2f568c
Martin <martin_lenart@icloud.com>
2023-09-11 10:08:53 +0200

    07 numbers added

---
+++ 07_numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter numbers separated by ; (1; 2; 4; )");
+
+string strInput = Console.ReadLine();
+string[] arrInput = strInput.Split(";");
+
+int sum = 0;
+for (int i = 0; i < arrInput.Length-1; i++)
+{
+    int intVal = int.Parse(arrInput[i]);
+    sum = sum + intVal;
+}
+
+Console.WriteLine($"The sum is {sum}");
