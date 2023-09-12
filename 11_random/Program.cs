d01f0eb05d0a5e289ee51daa7b9e13af8a3c9c1c
Martin <martin_lenart@icloud.com>
2023-09-12 13:54:19 +0200

    update

--- 11_random/Program.cs
+++ 11_random/Program.cs
@@ -5,12 +5,23 @@ class Program
     static void Main(string[] args)
     {
 
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < 10; i++)
         {
-            var name = GetName();
+            //Parameterless methods
+            //var name = GetName();
             string license = GetRegNr();
+            //string make = GetMake();
+            //string model = GetModel();
 
-            Console.WriteLine($"{name}, has a car with license number {license} ");
+            //Parameter Methods
+            string fName = GetElementFromString("Joakim, Josef, Emilja, Nicole, Samir, Amin, Bashir");
+            string lName = GetElementFromString("Emil, Eddie, Martin, Andre, Robin, Christos");
+            var name = $"{fName} {lName}";
+
+            string make = GetElementFromString("Volvo, BMW, Opel, Mini, VW, KIA, Toyota");
+            string model = GetElementFromString("Corolla, Golf, Rio, V70");
+
+            Console.WriteLine($"{name}, has a {make} {model} with license number {license} ");
          }
 
         Console.ReadKey();
@@ -44,6 +55,35 @@ class Program
         string license = $"{lc1}{lc2}{lc3} {i1}{i2}{i3}";
         return license;
     }
+    
+    static string GetMake()
+    {
+        string[] s = "Volvo, BMW, Opel, Mini, VW, KIA, Toyota".Split(", ");
 
+        Random rnd = new Random();
+        int i = rnd.Next(0, s.Length);
+        string make = s[i];
+        return make;
+    }
+
+    static string GetModel()
+    {
+        string[] s = "Corolla, Golf, Rio, V70".Split(", ");
+
+        Random rnd = new Random();
+        int i = rnd.Next(0, s.Length);
+        string make = s[i];
+        return make;
+    }
+
+    static string GetElementFromString(string str)
+    {
+        string[] s = str.Split(", ");
+
+        Random rnd = new Random();
+        int i = rnd.Next(0, s.Length);
+        string item = s[i];
+        return item;
+    }
 }
 
