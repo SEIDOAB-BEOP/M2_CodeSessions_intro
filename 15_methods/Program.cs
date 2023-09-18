1f07ba9bb478e229f5c356375628a46405b1e366
Martin <martin_lenart@icloud.com>
2023-09-18 14:13:08 +0200

    project 16 added

---
+++ 15_methods/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _15_methods;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayHello("Alina");
+        SayHello("Samir");
+
+        int res = AddNumbers(8, 10, 10, "123");
+        Console.WriteLine(res);
+
+        int a = 3, b = 4, c = 10;
+        int o;
+        int res1 = AddNumbers(a, b, out c, out o);
+
+        Console.WriteLine(res1);
+
+        Console.WriteLine(c);
+        Console.WriteLine(o);
+        
+    }
+
+    static void SayHello(string greeting)
+    {
+        greeting += " and Martin";
+        Console.WriteLine($"Hello {greeting}");
+    }
+
+    static int AddNumbers(int n1, int n2, int n3, string s)
+    {
+        return n1 + n2 + n3 + int.Parse(s);
+    }
+    static int AddNumbers(int n1, int n2, out int c, out int o)
+    {
+        c = 100;
+        o = 200;
+        return n1 + n2 + c;
+    }
+}
+
---
+++ 16_methods_tryparse/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _16_methods_tryparse;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number");
+        string s_nr = Console.ReadLine();
+
+        int nr;
+        bool all_ok;
+
+        nr = int.Parse(s_nr);
+        all_ok = int.TryParse(s_nr, out nr);
+
+        if (all_ok)
+        {
+            Console.WriteLine(nr + 10);
+        }
+        else
+        {
+            Console.WriteLine("Not a number");
+        }
+    }
+}
+
